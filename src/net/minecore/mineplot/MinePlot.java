24d50e353a158fa1bebb0a8add59904096ad3015
Francesco Macagno <fjmacagno1@yahoo.com>
2014-01-14 21:55:40 -0800

    Added new config value

--- src/net/minecore/mineplot/MinePlot.java
+++ src/net/minecore/mineplot/MinePlot.java
@@ -1,5 +1,5 @@
 package net.minecore.mineplot;
- 
+
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -26,111 +26,111 @@ public class MinePlot extends JavaPlugin {
 	private PlotPlayerManager mm;
 	private PlayerPlotSelector pps;
 	private PlotSeller plotSeller;
-	
+
 	@Override
-	public void onLoad(){
+	public void onLoad() {
 		log = this.getLogger();
-		
+
 		conf = this.getConfig();
 		conf.options().copyDefaults(true);
 		saveConf();
-		
+
 		conf.addDefault("tool", Material.BOWL.name());
-		
+		conf.addDefault("allowDataCollection", true);
+
 		mm = new PlotPlayerManager(this);
-		
+
 		pps = new PlayerPlotSelector(Material.matchMaterial(conf.getString("tool")));
 		plotSeller = new PlotSeller(this);
-		
+
 		log.info("Loaded!");
 	}
-	
+
 	@Override
-	public void onDisable(){
-		
-		for(PlotPlayer p : mm.getPlotPlayers())
-			if(!p.savePlots())
+	public void onDisable() {
+
+		for (PlotPlayer p : mm.getPlotPlayers())
+			if (!p.savePlots())
 				log.warning("Couldn't save data for player " + p.getName());
 	}
-	
-	public void save(){
-		
-		for(String name : pwm.getWorlds().keySet()){
+
+	public void save() {
+
+		for (String name : pwm.getWorlds().keySet()) {
 			log.info("Saving world " + name);
 			pwm.getPlotWorld(name).save(conf.getConfigurationSection("worlds." + name));
 		}
-		
+
 		log.info("Worlds saved");
-		
+
 		saveConf();
 	}
-	
-	private void loadWorlds(){
-		
+
+	private void loadWorlds() {
+
 		ConfigurationSection worlds = conf.getConfigurationSection("worlds");
-		if(worlds == null)
+		if (worlds == null)
 			worlds = conf.createSection("worlds");
-		
+
 		pwm = new PlotWorldManager();
-		
-		for(String s : worlds.getKeys(false)){
+
+		for (String s : worlds.getKeys(false)) {
 			World w;
-			if((w = this.getServer().getWorld(s)) == null)
+			if ((w = this.getServer().getWorld(s)) == null)
 				log.warning("Configuration values for world " + s + " can't be loaded because the world doesn't exist.");
-			else{
+			else {
 				log.info("Loading world " + s);
 				pwm.addPlotWorld(PlotWorld.getNewPlotWorld(worlds.getConfigurationSection(s), w));
 			}
 		}
 	}
-	
-	public boolean initWorld(World w){
-		
-		if(pwm.getPlotWorld(w) != null)
+
+	public boolean initWorld(World w) {
+
+		if (pwm.getPlotWorld(w) != null)
 			return false;
-		
+
 		ConfigurationSection worlds = conf.getConfigurationSection("worlds");
 		pwm.addPlotWorld(PlotWorld.getNewPlotWorld(worlds.createSection(w.getName()), w));
-		
+
 		saveConf();
-		
+
 		return true;
 	}
-	
-	private void saveConf(){
+
+	private void saveConf() {
 		try {
 			conf.save(this.getDataFolder().getPath() + File.separator + "config.yml");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
-	public void onEnable(){
-		
+	public void onEnable() {
+
 		loadWorlds();
-		
+
 		saveConf();
-		
-		
+
 		getCommand("plot").setExecutor(new PlotCommandInterpreter(pps, plotSeller, this));
 		getCommand("mineplot").setExecutor(new CommandInterpreter(this));
-		
-		
+
 		getServer().getPluginManager().registerEvents(new WorldListener(this), this);
 		getServer().getPluginManager().registerEvents(pps, this);
 		getServer().getPluginManager().registerEvents(plotSeller, this);
-		
-		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+
+		if (conf.getBoolean("allowDataCollection")) {
+			try {
+				Metrics metrics = new Metrics(this);
+				metrics.start();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
-		
+
 		log.info("Enabled!");
-		
-		
+
 	}
 
 	/**
@@ -145,14 +145,14 @@ public class MinePlot extends JavaPlugin {
 	}
 
 	public MineCore getMineCore() {
-		return ((MineCore)getServer().getPluginManager().getPlugin("MineCore"));
+		return ((MineCore) getServer().getPluginManager().getPlugin("MineCore"));
 	}
-	
-	public Material getSelectionTool(){
+
+	public Material getSelectionTool() {
 		return pps.getTool();
 	}
-	
-	public PlotSeller getPlotSeller(){
+
+	public PlotSeller getPlotSeller() {
 		return plotSeller;
 	}
 }
